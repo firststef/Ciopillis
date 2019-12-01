26616d685705ed036b148b80ef1a71481b84833c
firststef <firstbossspike@gmail.com>
2019-12-01 23:38:27 +0200

    Fixed build on windows

--- CardGame/main.cpp
+++ CardGame/main.cpp
@@ -16,7 +16,11 @@ int main()
 
     std::string gameLog;
 
-    CardGenerator generator("../GameServer/card_database.json");
+#ifdef WIN32
+    CardGenerator generator("D:/GameDev/Ciopillis/GameServer/card_database.json");
+#else
+	CardGenerator generator("/home/first/Documents/Ciopillis/GameServer/card_database.json");
+#endif
 
     Player player("Player", generator.container);
     Player computer("Computer", generator.container);
