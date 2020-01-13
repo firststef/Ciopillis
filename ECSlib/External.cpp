d89ac51966a0f68713d6cdc3eefa5f5eb8f0c75a
firststef <firstbossspike@gmail.com>
2020-01-13 03:21:35 +0200

    Fixed linux files

--- ECSlib/External.cpp
+++ ECSlib/External.cpp
@@ -1,11 +1,16 @@
 #ifdef WIN32
 #include <Windows.h>
+#elif __linux__
+#include <unistd.h>
+#include <bits/types/struct_timespec.h>
+#include <time.h>
+
 #endif
 void SleepFunc(unsigned long milliseconds) {
 #ifdef WIN32
 	Sleep(milliseconds);
 #elif _POSIX_C_SOURCE >= 199309L
-	struct timespec ts;
+	timespec ts;
 	ts.tv_sec = milliseconds / 1000;
 	ts.tv_nsec = (milliseconds % 1000) * 1000000;
 	nanosleep(&ts, NULL);
